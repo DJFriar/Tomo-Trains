fef17d9a568cee8c2a1dfe96e51f8278d166b670
DJFriar <tommy.craft@icloud.com>
2022-02-11 01:05:22 -0600

    added an endpoint to add/remove sample data.

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,6 @@
 const trains = require("../data/trains");
 
 module.exports = function (app) {
-  app.get("/", (req, res) => {
-    console.log("==== API / hit ====");
-    res.json("This endpoint is no longer in service.")
-  })
 
   app.post("/trains", (req, res) => {
     const TrainID = req.body.TrainID;
@@ -30,5 +26,23 @@ module.exports = function (app) {
     res.json("Next train info coming soon.")
   })
 
+  app.get("/sampledata/:toggle", (req, res) => {
+    const toggle = req.params.toggle;
 
+    if (toggle === "enable") {
+      trains["EUR6"] = ["1200", "1500", "1800", "2100"];
+      trains["X88B"] = ["0930", "1030", "1130"];
+      trains["TX77"] = ["0100", "1300"];
+      res.json({success: "Sample Data has been loaded."})
+    }
+    if (toggle === "disable") {
+      delete trains["EUR6"];
+      delete trains["X88B"];
+      delete trains["TX77"];
+      res.json({success: "Sample Data has been removed."})
+    }
+    if (toggle != "enable" && toggle != "disable") {
+      res.status(400).json({error: "Invalid request."});
+    }
+  })
 }
