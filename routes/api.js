862bb1126340252196d8c471991154b042893e08
DJFriar <tommy.craft@icloud.com>
2022-02-11 01:00:55 -0600

    Started the data handling.

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,34 @@
+const trains = require("../data/trains");
+
 module.exports = function (app) {
   app.get("/", (req, res) => {
     console.log("==== API / hit ====");
+    res.json("This endpoint is no longer in service.")
   })
 
-  app.get("/trains", (req, res) => {
+  app.post("/trains", (req, res) => {
+    const TrainID = req.body.TrainID;
+    const Times = req.body.Times;
+
     console.log("==== API /trains hit ====");
+    trains[TrainID] = Times;
+
+    res.json("We are not currently accepting new train lines.")
   })
 
   app.get("/trains/:id", (req, res) => {
     const id = req.params.id;
-    console.log("==== API /trains/" + id + " hit ====");
+
+    if (id.length != 4) {
+      res.status(400).json({error: "Invalid Train ID provided." });
+    }
+    res.status(400).json({error: "Invalid Train ID provided."})
   })
 
   app.get("/trains/next", (req, res) => {
     console.log("==== API /trains/next hit ====");
+    res.json("Next train info coming soon.")
   })
+
+
 }
